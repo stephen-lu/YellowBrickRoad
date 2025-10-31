c40cd74648c1e514761e54dc0c35ce85dc052a71
Stephen <stephen.lu20@gmail.com>
2025-10-30 23:31:47 -0400

    basically done house. could add more later

--- DreamHouse2025/src/main/java/rocks/zipcode/dreamhouse/Picture.java
+++ DreamHouse2025/src/main/java/rocks/zipcode/dreamhouse/Picture.java
@@ -29,30 +29,67 @@ public class Picture
      */
     public void draw()
     {
-        drawTower(50, 50);
-        drawTower(750, 50);
-        drawTowerNoTopper(350, 50);
-        drawTowerNoTopper(450, 50);
-        drawRoof(250, 25);
-        drawWall(150, 50);
-        drawWall(250, 50);
-        drawWall(550, 50);
-        drawWall(650, 50);
-        drawBorders(0, 0);
-        drawDoor(0, 0);
+        drawCastle(50, 0);
+    }
+
+    public void drawCastle(int xOffset, int yOffset)
+    {
+        drawGrass();
+        drawWater();
+        drawTower(50 + xOffset, 50 + yOffset);
+        drawTower(750 + xOffset, 50 + yOffset);
+        drawTowerNoTopper(350 + xOffset, 50 + yOffset);
+        drawTowerNoTopper(450 + xOffset, 50 + yOffset);
+        drawRoof(250 + xOffset, 25 + yOffset);
+        drawWall(150 + xOffset, 50 + yOffset);
+        drawWall(250 + xOffset, 50 + yOffset);
+        drawWall(550 + xOffset, 50 + yOffset);
+        drawWall(650 + xOffset, 50 + yOffset);
+        drawBorders(0 + xOffset, 0 + yOffset);
+        drawDoor(0 + xOffset, 0 + yOffset);
+        drawWindows(200 + xOffset, 175 + yOffset);
+        drawWindows(600 + xOffset, 175 + yOffset);
+    }
+
+    public void drawWater ()
+    {
+        RectangleZC water = new RectangleZC(200, 200, 400, 300, new Color(0, 121, 180));
+        water.makeVisible();
+    }
+
+    public void drawGrass ()
+    {
+        RectangleZC grass = new RectangleZC(1000, 200, 0, 300, new Color(124, 252, 0));
+        grass.makeVisible();
+    }
+
+    public void drawWindows (int xOffset, int yOffset)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            drawWindow(80 * (i % 2) + xOffset, 100 * (i/2) + yOffset);
+        }
+    }
+
+    public void drawWindow (int xOffset, int yOffset)
+    {
+        Square window = new Square(30, 0 + xOffset, 15 + yOffset, Color.black);
+        window.makeVisible();
+        Circle windowTop = new Circle(30, 0 + xOffset, 0 + yOffset, Color.black);
+        windowTop.makeVisible();
     }
 
     public void drawDoor(int xOffset, int yOffset)
     {
-        RectangleZC door = new RectangleZC(100, 150, 400, 225, new Color(127, 81, 18));
+        RectangleZC door = new RectangleZC(100, 150, 400 + xOffset, 225 + yOffset, new Color(127, 81, 18));
         door.makeVisible();
-        Circle doorTop = new Circle(100, 400, 175, new Color(127, 81, 18));
+        Circle doorTop = new Circle(100, 400 + xOffset, 175 + yOffset, new Color(127, 81, 18));
         doorTop.makeVisible();
-        RectangleZC doorLineVertical = new RectangleZC(4, 200, 448, 175, Color.darkGray);
+        RectangleZC doorLineVertical = new RectangleZC(4, 200, 448 + xOffset, 175 + yOffset, Color.darkGray);
         doorLineVertical.makeVisible();
-        RectangleZC doorLineHorizontal1 = new RectangleZC(100, 4, 400, 325, Color.darkGray);
+        RectangleZC doorLineHorizontal1 = new RectangleZC(100, 4, 400 + xOffset, 325 + yOffset, Color.darkGray);
         doorLineHorizontal1.makeVisible();
-        RectangleZC doorLineHorizontal2 = new RectangleZC(100, 4, 400, 250, Color.darkGray);
+        RectangleZC doorLineHorizontal2 = new RectangleZC(100, 4, 400 + xOffset, 250 + yOffset, Color.darkGray);
         doorLineHorizontal2.makeVisible();
     }
 
